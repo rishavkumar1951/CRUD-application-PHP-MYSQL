e641cbeb95964be04e693076fd84740bdbbdccd8
rishavkumar1951 <57628736+rishavkumar1951@users.noreply.github.com>
2021-10-29 16:43:56 +0530

    Add files via upload

---
+++ delete.php
@@ -0,0 +1,26 @@
+<?php include 'header.php'; 
+if(isset($_POST['deletebtn'])){
+
+    include "config.php";
+    $stu_id=$_POST['sid'];
+    $sql="DELETE FROM student WHERE sid={$stu_id}";
+    $result=mysqli_query($conn, $sql) or die ("Query Unsuccessful");
+    header("location: http://localhost/crud/index.php");
+    mysqli_close($conn);
+}
+?>
+
+
+<div id="main-content">
+    <h2>Delete Record</h2>
+    <form class="post-form" action="" method="post">
+        <div class="form-group">
+            <label>Id</label>
+            <input type="text" name="sid" />
+        </div>
+        <input class="submit" type="submit" name="deletebtn" value="Delete" />
+    </form>
+</div>
+</div>
+</body>
+</html>
---
+++ updatedata.php
@@ -0,0 +1,17 @@
+<?php
+$stu_id = $_POST['sid'];
+$stu_name = $_POST['sname'];
+$stu_address = $_POST['saddress'];
+$stu_class = $_POST['sclass'];
+$stu_phone = $_POST['sphone'];
+
+$conn = mysqli_connect("localhost", "root", "", "crud") or die("Connection Failed");
+$sql = "UPDATE student SET sname='{$stu_name}', saddress='{$stu_address}',sclass='{$stu_class}',sphone='{$stu_phone}' WHERE sid={$stu_id}";
+$result = mysqli_query ($conn, $sql) or die("Query Unsuccessful.") ;
+
+// redirecting to index.php                         
+header("location: http://localhost/crud/index.php");
+
+// closing our connection
+mysqli_close($conn);
+?>
